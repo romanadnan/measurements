4d358639c35e91ef86349c345925c2ad2ef31293
Roman Ahmed Adnan <44108563+romanadnan@users.noreply.github.com>
2024-09-04 12:07:14 +0600

    Work on header

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+// alert( 'hello' );
+
+// function shwoContant() {
+//     document.getElementById('header').innerHTML('Roman')
+
+    
+// }
+
+// document.getElementsByTagName('img').src = '../assets/img/logo.png';
+// document.getElementById('header').innerHTML(`<img src="assets/img/logo.png" alt="">`);
+
+
+
+
+// document.getElementById("header").innerHTML = `<img src="assets/img/logo.png" alt="">`;
+
+
+// $(function(){
+//     $("#header").load("header.html");
+//   });
+
+// let wgthost = document.getElementById("header");
+// wgthost.appendChild(new HTMLInclude("template-parts/header.html"));
